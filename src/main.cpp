7ecf1af995d54e948fcb7f84da7e53cfea1fd60e
niurenpeng <niurenpeng@niurenpeng.com>
2015-12-31 13:05:20 +0800

    修改调用测试用例方法

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,28 @@
 #include <stdio.h>
 #include "Tokenizer.h"
 #include "Parser.h"
-int main() {
-    Tokenizer tokenizer("../dataset/weather/weather.input", false);
-    Parser parser("../dataset/weather.aql", tokenizer);
-    //Tokenizer tokenizer("../dataset/perloc/PerLoc.input");
-    //Parser parser("../dataset/PerLoc.aql", tokenizer);
-    parser.program();
+int main(int argc, const char * argv[]) {
+	cout << endl << argc << endl;
+	if (argc == 1) {
+		Tokenizer tokenizer("../dataset/weather/weather.input", false);//输入文本
+    	Parser parser("../dataset/weather.aql", tokenizer);//aql 文件，输入文本的 tokenizer
+    	parser.program();
+	} else if(argc == 2){
+		if (argv[1][0] == '1') {
+   			//测试用例1：Perloc
+    		Tokenizer tokenizer("../dataset/perloc/PerLoc.input", false);
+    		Parser parser("../dataset/PerLoc.aql", tokenizer);
+    		parser.program();
+		} else if (argv[1][0] == '2'){
+			//测试用例2：revenue
+    		Tokenizer tokenizer("../dataset/revenue/Revenue.input", false);//输入文本
+    		Parser parser("../dataset/Revenue.aql", tokenizer);//aql 文件，输入文本的 tokenizer
+    		parser.program();
+		} else {
+			cout << "wrong argument 2!" << endl;
+		}
+	} else {
+		cout << "wrong argument muti!" << endl;
+	}
     return 0;
 }
